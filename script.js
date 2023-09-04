446b0e84da9739980e360c3b89fa15e34800c16d
robby <5150reb@gmail.com>
2023-09-04 09:36:34 -0400

    basic working addBooksToDisplay function
    - bug: addBooksToDisplay loops through entire array and re-adds books already present in the library to the display again

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//query selectors
+const libraryDisplay = document.querySelector('.library-display');
+
+
+//create elements
+
+
+
+
 const myLibrary = [];
 
 function Book(title, author, pageCount, readCheck) {
@@ -9,12 +18,31 @@ function Book(title, author, pageCount, readCheck) {
 
 Book.prototype.addBookToLibrary = function() {
   myLibrary.push(this);
+  addBooksToDisplay();
+}
+
+function addBooksToDisplay() {
+  for (i = 0; i < myLibrary.length; i++) {
+    let bookCell = document.createElement('div');
+    let bookTitle = document.createElement('p');
+    bookTitle.textContent = myLibrary[i].title;
+    let bookAuthor = document.createElement('p');
+    bookAuthor.textContent = myLibrary[i].author;
+    let bookPageCount = document.createElement('p');
+    bookPageCount.textContent = myLibrary[i].pageCount;
+    let bookReadCheck = document.createElement('button');
+    bookReadCheck.textContent = myLibrary[i].readCheck;
+    if (bookReadCheck.textContent === 'read') {
+      bookReadCheck.classList.toggle('read');
+    } else bookReadCheck.classList.toggle('not-read');
+    bookCell.appendChild(bookTitle);
+    bookCell.appendChild(bookAuthor);
+    bookCell.appendChild(bookPageCount);
+    bookCell.appendChild(bookReadCheck);
+    bookCell.classList.add('book-cell');
+    libraryDisplay.appendChild(bookCell);
+  }
 }
 
 const newBook = new Book('fellowship', 'jrr', 200, 'not read');
 const newBook2 = new Book('ex', 'test', 13, 'read');
-
-newBook.addBookToLibrary();
-newBook2.addBookToLibrary();
-
-console.log(myLibrary);
