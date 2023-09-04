b02e4f13b8bcb8197c3a4089c13287510128d970
robby <5150reb@gmail.com>
2023-09-04 09:02:51 -0400

    create constructor function, push book to list
    - constructor function to make book objects
    - addBookToLibrary function pushes book to myLibrary

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const myLibrary = [];
 
-function Book() {
-  //stuff here
+function Book(title, author, pageCount, readCheck) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.readCheck = readCheck;
 }
 
-function addBookToLibrary() {
-  //stuff here
+Book.prototype.addBookToLibrary = function() {
+  myLibrary.push(this);
 }
+
+const newBook = new Book('fellowship', 'jrr', 200, 'not read');
+const newBook2 = new Book('ex', 'test', 13, 'read');
+
+newBook.addBookToLibrary();
+newBook2.addBookToLibrary();
+
+console.log(myLibrary);
